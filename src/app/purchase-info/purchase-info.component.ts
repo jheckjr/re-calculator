528de75cab26f35c7520f7d37e50422ccc8d8b89
jheckjr <joel.heckjr@gmail.com>
2017-06-29 03:31:18 +0000

    Fix purchase info calculations and display

--- src/app/purchase-info/purchase-info.component.ts
+++ src/app/purchase-info/purchase-info.component.ts
@@ -49,32 +49,43 @@ export class PurchaseInfoComponent implements OnChanges {
     }
   }
   
-  // TODO: make these functions populate component variables
   // Total cost of cash purchase
   private totalCost() {
-  	return (this.purchaseInfo.purchasePrice + this.purchaseInfo.closingCosts + 
+  	const cost = (this.purchaseInfo.purchasePrice + this.purchaseInfo.closingCosts + 
   	  this.purchaseInfo.repairCosts);
+  	const strCost = cost.toFixed(0);
+  	return '$' + Number(strCost).toLocaleString();
   }
   
   // Downpayment amount
   private downPmtAmt() {
-  	return (this.purchaseInfo.purchasePrice * this.purchaseInfo.loanInfo.downPmtPct / 100);
+  	const downPmt = (this.purchaseInfo.purchasePrice * this.purchaseInfo.loanInfo.downPmtPct / 100) || 0;
+  	const strDownPmt = downPmt.toFixed(0);
+  	return '$' + Number(strDownPmt).toLocaleString();
   }
   
   // Total loan amount
   private loanAmt() {
-  	return (this.purchaseInfo.purchasePrice * 
-  	  (100 - this.purchaseInfo.loanInfo.downPmtPct) / 100);
+  	const loanAmt = (this.purchaseInfo.purchasePrice * 
+  	  (100 - this.purchaseInfo.loanInfo.downPmtPct) / 100) || 0;
+  	const strLoanAmt = loanAmt.toFixed(0);
+  	return '$' + Number(strLoanAmt).toLocaleString();
   }
   
   // Monthly loan payment
-  private loanPmtAmt() {
-    let rate = (this.purchaseInfo.loanInfo.interestRate / 100) / 12;	// monthly rate
-    let term = this.purchaseInfo.loanInfo.loanTerm * 12;	// term in months
-    let loanAmt = loanAmt();
-    let payment = (loanAmt * rate * Math.pow(1 + rate, term)) /
-      (Math.pow(1 + rate, term) - 1);
-
-    return payment;
+  private loanPmt() {
+    const rate = (this.purchaseInfo.loanInfo.interestRate / 100) / 12;	// monthly rate
+    const term = this.purchaseInfo.loanInfo.loanTerm * 12;	// term in months
+    const loanAmt = (this.purchaseInfo.purchasePrice * 
+  	  (100 - this.purchaseInfo.loanInfo.downPmtPct) / 100);
+    
+    let payment = ( (loanAmt * rate * Math.pow(1 + rate, term)) /
+      (Math.pow(1 + rate, term) - 1) );
+    if (!isFinite(payment)) {
+    	payment = 0;
+    }
+    
+    const strPayment = payment.toFixed(0);
+    return '$' + Number(strPayment).toLocaleString();
   }
 }
